81ea16aafd6b855ce3ffa2a1f051747b54e3ea08
Bably <bablyd15@gmail.com>
2021-08-29 22:39:53 +0530

    checkbox and group list added

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     const taskData = document.querySelector("#exampleInput1").value;
     console.log(taskData);
-    const taskList = document.querySelector("#task");
-    taskList.insertAdjacentHTML("beforeEnd", `<li>${taskData}</li>`);
+    const taskList = document.querySelector(".task");
+    taskList.insertAdjacentHTML(
+        "beforeEnd",
+        `<li class="list-group-item me-4"><input class="form-check-input " type="checkbox" value="" aria-label="Checkbox for following text input">${taskData}</li>`
+    );
 });
